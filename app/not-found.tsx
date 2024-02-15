62ae8b4beb38acb60786836009ecbae50e1102f8
quanosek <kubaklalo@gmail.com>
2024-02-15 21:59:07 +0100

    not-found page styling

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,30 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 import styles from "@/styles/not-found.module.scss";
 
 export default function NotFoundPage() {
+  const router = useRouter();
+  const [seconds, setSeconds] = useState(10); // 10 seconds
+
+  useEffect(() => {
+    const counter = setInterval(() => {
+      setSeconds((prevSeconds: number) => prevSeconds - 1);
+      if (seconds === 1) router.push("/");
+    }, 1000);
+
+    return () => clearInterval(counter);
+  }, [router, seconds]);
+
   return (
-    <main>
-      <h1>Nie znaleziono strony</h1>
+    <main className={styles.notFound}>
+      <h1>Nie znaleziono strony!</h1>
+
+      <p className={styles.description}>
+        Powrót do generatora nastąpi za [{seconds}] sekund
+      </p>
     </main>
   );
 }
